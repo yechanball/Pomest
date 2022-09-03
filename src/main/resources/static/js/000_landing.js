6899c6e2a59a72e33aa93c8feabdbfcd5e3a0653
Yechan <77843815+yechanball@users.noreply.github.com>
2022-09-03 23:48:15 +0900

    refactor: Refactor landing page

--- src/main/resources/static/js/000_landing.js
+++ src/main/resources/static/js/000_landing.js
@@ -1,13 +1,32 @@
-function loginKakao(){
+// 카카오톡 소셜 로그인 버튼 클릭
+document.querySelector("#btn-login-kakao").addEventListener("click", function () {
     //////////////////////////////////////////////
+    // 카카오톡 로그인 기능 구현부
     // location.href = '/oauth2/authorization/kakao';
     //////////////////////////////////////////////
     location.href='./011_inputNickname.html';
-}
+});
 
-function loginNaver(){
+// 네이버 소셜 로그인 버튼 클릭
+document.querySelector("#btn-login-naver").addEventListener("click", function () {
     //////////////////////////////////////////////
+    // 네이버 로그인 기능 구현부
     // location.href = '/oauth2/authorization/naver';
-    ///////////////////////////////////////////////
+    //////////////////////////////////////////////
     location.href='./011_inputNickname.html';
-}
+});
+
+// 로그인이 필요한 이유 클릭 -> 바텀시트 열기 (아래 -> 위 500ms)
+var botSheet = document.querySelector(".bottom-sheet");
+document.querySelector("#btn-open-bottomsheet").addEventListener("click", function () {
+    botSheet.classList.remove("animate__animated", "animate__slideOutDown", "animate__faster");
+    botSheet.classList.add("animate__animated", "animate__slideInUp", "animate__faster");
+    botSheet.style.display = "block";
+});
+
+// X 클릭 -> 바텀시트 닫기 (위 -> 아래 500ms)
+document.querySelector("#btn-close-bottomsheet").addEventListener("click", function () {
+    botSheet.classList.remove("animate__animated", "animate__slideInUp", "animate__faster");
+    botSheet.classList.add("animate__animated", "animate__slideOutDown", "animate__faster");
+    // botSheet.style.display = "none";
+});
--- src/main/resources/static/js/012_inputDefaultInfo.js
+++ src/main/resources/static/js/012_inputDefaultInfo.js
@@ -1,7 +1,24 @@
-window.onload = function() {
+// 페이지 로딩시 닉네임 호출
+window.onload = function () {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     var nick =urlParams.get('nick');
-
-    document.all.titleText.innerHTML = nick+"님께 필요한 정보를<br>꼼꼼히 모으기 위해<br>이런 것들을 알고 싶어요";
+    
+    if (nick == null) { // 닉네임이 전달되지 않은 경우
+        // 에러 처리하기
+        alert("닉네임이 설정되지 않았습니다!!!");
+        location.href='./011_inputNickname.html';
+    } else {
+        document.querySelector("#title-text").innerHTML = nick+"님께 필요한 정보를<br>꼼꼼히 모으기 위해<br>이런 것들을 알고 싶어요";
+    }
 };
+
+// 다음에 할게요 버튼 클릭 -> 2 포스트 페이지로 이동
+document.querySelector("#btn-do-next").addEventListener("click", function () {
+    location.href = './200_post.html';
+});
+
+// 지금 입력할게요 버튼 클릭 -> 013 기본정보 입력 페이지로 이동
+document.querySelector("#btn-do-now").addEventListener("click", function () {
+    location.href = './013_inputInfoDetail.html';
+});
